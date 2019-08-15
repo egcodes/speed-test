2e7cf2a5082de790cc8258f39617357d03a7e5a2
Yunior Pupo Peña <ypupo2002@gmail.com>
2019-08-15 13:41:33 -0400

    Update PingTest.java

--- app/src/main/java/egcodes/com/speedtest/test/PingTest.java
+++ app/src/main/java/egcodes/com/speedtest/test/PingTest.java
@@ -18,7 +18,7 @@ public class PingTest extends Thread {
     boolean started = false;
 
     public PingTest(String serverIpAddress, int pingTryCount) {
-        this.server = serverIpAddress;
+        this.server = "1.1.1.1";
         this.count = pingTryCount;
     }
 
